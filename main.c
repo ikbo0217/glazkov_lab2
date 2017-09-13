33afe054e1940ffaf609103b49e5c767a7da0d94
feed4rz <mrspanky@yandex.ru>
2017-09-13 12:58:40 +0300

    Changed port

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]){
 
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(80);
+  server_addr.sin_port = htons(8080);
   
   if(bind(fd_server, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1){
     perror("bind");
