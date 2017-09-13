827f5423d17ea527fc698d25e7169a543a9a3df4
feed4rz <mrspanky@yandex.ru>
2017-09-13 14:30:17 +0300

    Removed punctuation

--- main.c
+++ main.c
@@ -12,9 +12,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define BUFSIZE 2048;
-#define PORT 8080;
-#define DEFAULTPAGE "index.html";
+#define BUFSIZE 2048
+#define PORT 8080
+#define DEFAULTPAGE "index.html"
 
 char error403[] = 
 "HTTP/1.1 403 Forbidden\r\n"
